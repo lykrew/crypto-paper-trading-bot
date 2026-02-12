fedc378b0724829866e054fd3f34bc64acf3f0b3
lykrew <lykrew@gmail.com>
2026-02-12 17:22:22 +0300

    add buy command

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const { getPrice } = require('./services/binance_service');
+const { getUser, updateUser } = require('./services/user_storage.js');
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
@@ -24,5 +25,41 @@ bot.onText(/\/price (.+)/, async (msg, match) => {
         bot.sendMessage(chatId, 'Ошибка. Возможно, такой монеты не существует.')
     }
 })
+
+bot.onText(/\/buy (.+) (\d+)/, async (msg, match) => {
+    const chatId = msg.chat.id;
+    const symbol = match[1].toUpperCase();
+    const amount = Number(match[2]);
+
+    try {
+        const price = await getPrice(symbol);
+        const user = getUser(chatId);
+
+        if (user.balance < amount) {
+            return bot.sendMessage(chatId, 'Недостаточно средств 💸');
+        }
+
+        const quantity = amount / price;
+
+        user.balance -= amount;
+
+        if (!user.portfolio[symbol]) {
+            user.portfolio[symbol] = 0;
+        }
+
+        user.portfolio[symbol] += quantity;
+
+        updateUser(chatId, user);
+
+        bot.sendMessage(
+            chatId,
+            `✅ Куплено ${quantity.toFixed(6)} ${symbol}\nОстаток: ${user.balance.toFixed(2)}$`
+        );
+    } catch (e) {
+        bot.sendMessage(chatId, 'Ошибка при покупке.')
+    }
+});
+
+
 
 console.log('Bot is running...');
