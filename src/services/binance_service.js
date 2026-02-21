70645bb9b5ead92e8c02a3cb098d3c58e743294c
lykrew <lykrew@gmail.com>
2026-02-21 17:53:56 +0300

    fix

--- src/services/binance_service.js
+++ src/services/binance_service.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function getPrice(symbol) {
     const url = `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}USDT`;
-    
+
     const response = await axios.get(url);
 
     return response.data.price;
