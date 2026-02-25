47603938d8013ad9663f4bf2631a9f4040767628
lykrew <lykrew@gmail.com>
2026-02-25 16:46:44 +0300

    add better realization binance_service

--- src/services/binance_service.js
+++ src/services/binance_service.js
@@ -1,11 +1,20 @@
 const axios = require('axios');
 
 async function getPrice(symbol) {
-    const url = `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}USDT`;
+    // Делаем символ заглавным
+    symbol = symbol.toUpperCase();
+
+    // Если пользователь уже написал USDT — не добавляем повторно
+    if (!symbol.endsWith('USDT')) {
+        symbol = symbol + 'USDT';
+    }
+
+    const url = `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`;
 
     const response = await axios.get(url);
 
-    return response.data.price;
+    return Number(response.data.price);
 }
+
 
 module.exports = { getPrice };
